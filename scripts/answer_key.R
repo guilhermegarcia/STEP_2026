baa5e1e0ae9adc48eb86409a2ae3cc2c73e11230
guilhermegarcia <guilherme.garcia@lli.ulaval.ca>
2026-06-14 15:28:53 -0600

    updated scripts with answers to survey

---
+++ scripts/answer_key.R
@@ -0,0 +1,12 @@
+responses <- c(
+  "x == \"hello\"",
+  "mutate()",
+  "select()",
+  "filter()",
+  "a numeric vector",
+  "geom_point()",
+  "the median",
+  "means and standard errors",
+  "the resulting data object is not tidy",
+  "it specifies the mappings between variables and visual properties"
+)
